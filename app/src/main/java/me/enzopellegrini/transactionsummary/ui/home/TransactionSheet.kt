2514c9973549c53c950151570f0aaeb1202ead41
Vincenzo Pellegrini <vincenzo.pellegrini.2001@gmail.com>
2021-12-03 03:54:36 +0100

    Being bad with design was a given Deleted some files and the bottom sheet works perfectly now (it does not make sense in terms of UX)

--- app/src/main/java/me/enzopellegrini/transactionsummary/ui/home/TransactionSheet.kt
+++ app/src/main/java/me/enzopellegrini/transactionsummary/ui/home/TransactionSheet.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import me.enzopellegrini.transactionsummary.R
 import me.enzopellegrini.transactionsummary.data.Transaction
+import me.enzopellegrini.transactionsummary.databinding.TransactionSheetContentBinding
 
 class TransactionSheet(val transaction: Transaction) : BottomSheetDialogFragment() {
 
@@ -15,11 +16,13 @@ class TransactionSheet(val transaction: Transaction) : BottomSheetDialogFragment
         container: ViewGroup?,
         bundle: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.transaction_sheet_content, container, false)
+        val binding = TransactionSheetContentBinding.inflate(inflater, container, false)
 
+        binding.transactionName.text = transaction.name
+        binding.transactionAmount.text  = transaction.amount.toString()+"$" // TODO: Proper Currency Formatting
+        binding.transactionCategory.text = transaction.category
 
-
-        return view
+        return binding.root
     }
 
 
